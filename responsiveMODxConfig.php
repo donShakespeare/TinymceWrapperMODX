e700c869ba4907ffaddace6bdf915ef6a425503c
donShakespeare <donshakespeare@gmail.com>
2015-06-30 18:02:04 -0400

    Update responsiveMODxConfig.php

--- responsiveMODxConfig.php
+++ responsiveMODxConfig.php
@@ -1,8 +1,12 @@
 <?php
-// /assets/components/tinymce_wrapper/responsiveMODxConfig.php
+/*File location: assets/components/tinymce_wrapper/responsiveMODxConfig.php
+use freely, recode freely, report freely, enjoy freely
+
+http://www.leofec.com/modx-revolution/
+-donshakespeare in the MODx forum*/
 
 /*
-Before you use, go to
+Before you use, go to this include file
 - /assets/components/tinymce_wrapper/responsivefilemanager/filemanager/config/config.php
 - DELETE session_start(); (line 2)
 - INSERT include '../../responsiveMODxConfig.php' BEFORE return array_merge() (line338);
@@ -25,43 +29,68 @@ if ($modx->user->isMember('Administrator')) {
 else {
   die('<div style="position: fixed; margin: auto;width: 400px;height:200px;text-align:center;top:0;bottom:0;left:0;right:0;"><h1>IT SEEMS YOU DO NOT HAVE PERMISSION TO USE THIS COOL MANAGER</h1></div>');
 }
-$autoCreateUserFolders = false;
+
+/*  
+-Make sure your upload media folder already exists; for this sample we will use one already in this plugin's folder (assets/components/tinymce_wrapper/uploadMedia/)
+-Now calculate how to get to your folder from tinymce_wrapper/responsivefilemanager/filemanager/
+-When using MODxs Media Sources and all, and not sure how your client does stuff, use PHP's realpath to auto calculate this relationship 
+
+-To create a new location, follow the strict example below that is commented out
+*/
+
+$absolutePathtoUploadFolder = MODX_ASSETS_URL . 'components/tinymce_wrapper/uploadMedia/' . $personalFolder;
+$relativePathToUploadFolder = '../../uploadMedia/';
+$relativePathToUploadFolderThumbs = '../../uploadMediaThumbs/'; //must be outside uploadMedia folder
+$folderMessUrl =  MODX_ASSETS_URL."components/tinymce_wrapper/uploadMedia/"; //appears in the dialogue box
+
+//*****HOW TO CHANGE LOCATION
+/*$absolutePathtoUploadFolder = MODX_ASSETS_URL . 'uploadNew/'; //to access /assets/uploadNew/
+$relativePathToUploadFolder = '../../../../uploadNew/'; //to access /assets/uploadNew/
+$relativePathToUploadFolderThumbs = '../../../../newThumbs/'; //to access /assets/newThumbs/
+$folderMessUrl =  MODX_ASSETS_URL."uploadNew/";*/
+
 // auto create custom folders for each user; Example of sweet stuff you can do
+$autoCreateUserFolders = false;
 if ($autoCreateUserFolders == 1) {
   //if you ever find the MODx Media Source api, check permissions of each user/usergroup, then display Media Source path accordingly
   //grab id and username
   $modxId = $modx->user->get('id'); //you can use usergroup instead of userid
   $myUsername = $modx->user->get('username');
   //create special folder for certain admins
-  if ($modxId == 1 || $modxId == 6) {
+  if ($modx->user->isMember('Administrator')) {
+  // if ($modxId == 1 || $modxId == 6) {
     $personalFolder = "admin";
   } 
   else {
+  //everyone else gets a unique folder; you can use a safer criterion other than username if you like; encode, shorten, anything you can think of
     $personalFolder = $modx->user->get('username');
   }
   $personalFolder = strtolower($personalFolder);
   $xters = array(" ", "_");
   $personalFolder = str_replace($xters, "-", $personalFolder); //clean up the name
-  //setup MODx folder variables
-  //make sure your upload media folder exists
+
+  /*
+  -Each user/usergroup can have a config.php copied to their folder for further fine tuning of security
+  -Here, we will copy a template .tpl over each time we find one lacking .php
+  */
   $userSpecificConfigTemplate = '../../responsivePersonalConfig.tpl';
-  $userSpecificConfig = '../../uploadMedia/' . $personalFolder . '/config.php';
-  if (!file_exists('../../uploadMedia/' . $personalFolder)) {
-    mkdir('../../uploadMedia/' . $personalFolder, 0755, true);
+  $userSpecificConfig = $relativePathToUploadFolder . $personalFolder . '/config.php';
+  if (!file_exists($relativePathToUploadFolder . $personalFolder)) {
+    mkdir($relativePathToUploadFolder . $personalFolder, 0755, true);
   }
-  if (!file_exists('../../uploadMedia/' . $personalFolder . '/mp3')) {
-    mkdir('../../uploadMedia/' . $personalFolder . '/mp3', 0755, true);
+  if (!file_exists($relativePathToUploadFolder . $personalFolder . '/mp3')) {
+    mkdir($relativePathToUploadFolder . $personalFolder . '/mp3', 0755, true);
   }
-  if (!file_exists('../../uploadMedia/' . $personalFolder . '/pictures')) {
-    mkdir('../../uploadMedia/' . $personalFolder . '/pictures', 0755, true);
+  if (!file_exists($relativePathToUploadFolder . $personalFolder . '/pictures')) {
+    mkdir($relativePathToUploadFolder . $personalFolder . '/pictures', 0755, true);
   }
-  //the boss does not need this
-  //for further restriction, copy a personal config.php to every user/usergroup's folder
-  // if ($modxId !== 1)
-  // {
-  if (!file_exists('../../uploadMedia/' . $personalFolder . '/config.php')) {
+  //the overall boss does not need this new config
+  if ($modxId !== 1)
+  // if ($modx->user->isMember('Administrator'))
+  {
+  if (!file_exists($relativePathToUploadFolder . $personalFolder . '/config.php')) {
     copy($userSpecificConfigTemplate, $userSpecificConfig);
-    // }
+    }
   }
   //$config['upload_dir'] must have trailing slash
   if ($personalFolder) {
@@ -71,9 +100,9 @@ if ($autoCreateUserFolders == 1) {
     $personalFolder = '/';
   }
 }
-$config['upload_dir'] = MODX_ASSETS_URL . 'components/tinymce_wrapper/uploadMedia/' . $personalFolder;
-//how do I get to tinymce_wrapper/uploadMedia/ from tinymce_wrapper/responsivefilemanager/filemanager/  you may use PHP realpath to auto calculate this relationship
-$config['current_path'] = '../../uploadMedia/' . $personalFolder;
-$config['thumbs_base_path'] = '../../uploadMediaThumbs/'; //must be outside uploadMedia folder
-$folder_message = "Hello " . $myUsername . ", this is your folder (".MODX_ASSETS_URL."components/tinymce_wrapper/uploadMedia/" . $personalFolder . "). You may upload images. Right-click items for more info...";
+
+$config['upload_dir'] = $absolutePathtoUploadFolder . $personalFolder;
+$config['current_path'] = $relativePathToUploadFolder . $personalFolder;
+$config['thumbs_base_path'] = $relativePathToUploadFolderThumbs;
+$folder_message = "Hello " . $myUsername . ", this is your folder (" . $folderMessUrl . $personalFolder . "). You may upload images. Right-click items for more info...";
 ?>
